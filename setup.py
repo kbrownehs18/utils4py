921673cc618b53b5bc602c848d4451d32fc070f5
scnjl <scnjl@qq.com>
2020-03-22 10:51:15 +0800

    add Arial.ttf

--- setup.py
+++ setup.py
@@ -2,18 +2,14 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='utils4py',
-    version='0.0.11',
+    name="utils4py",
+    version="0.0.16",
     packages=find_packages(),
-    url='https://github.com/kbrownehs18/utils4py',
-    license='MPL',
-    author='last911',
-    author_email='scnjl@qq.com',
-    description='Python tools',
-    install_requires=[
-        'sqlalchemy',
-        'pycrypto',
-        "flask-wtf",
-        "pillow"
-    ]
+    include_package_data=True,
+    url="https://github.com/kbrownehs18/utils4py",
+    license="MPL",
+    author="last911",
+    author_email="scnjl@qq.com",
+    description="Python tools",
+    install_requires=["sqlalchemy", "pycrypto", "flask-wtf", "pillow"],
 )
