7d9a7c140c9145f0be9f8986ab31b76e038101b5
scnjl <scnjl@qq.com>
2020-11-17 13:31:24 +0800

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="utils4py",
-    version="0.0.24",
+    version="0.0.25",
     packages=find_packages(),
     include_package_data=True,
     url="https://github.com/kbrownehs18/utils4py",
